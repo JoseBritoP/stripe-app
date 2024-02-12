ddd06808008fd3fa6e21b0793a3363c905059368
José Brito <josealbritose@gmail.com>
2024-02-12 17:03:21 -0300

    Favorites Controllers

---
+++ src/app/api/controllers/favorites/index.ts
@@ -0,0 +1,7 @@
+import { postFavorites } from "./01 - postFavorites";
+import { removeFavorites } from "./02 - removeFavorites";
+import { getFavorites,getFavoritesByUser } from "./03 - getFavorites";
+
+export {
+  postFavorites,removeFavorites,getFavorites,getFavoritesByUser
+}
