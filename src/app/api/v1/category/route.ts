a06daf7a30d8fcc9eacf7f6e1395d4cd23f6f4d7
José Brito <josealbritose@gmail.com>
2024-02-12 17:36:34 -0300

    Category route

--- src/app/api/v1/category/route.ts
+++ src/app/api/v1/category/route.ts
@@ -1,9 +1,24 @@
-import { NextRequest,NextResponse } from "next/server";
+import { NextRequest,NextResponse as res } from "next/server";
 
-export async function GET(req:NextRequest){
-  return NextResponse.json({DIY:'All categories'})
+import { createCategory,getCategories } from "../../controllers/category";
+
+export async function GET(_req:NextRequest){
+  try {
+    const categories = await getCategories();
+    return res.json(categories,{status:200,statusText:'Categories OK'})
+  } catch (error:any) {
+    return res.json({error:error.message})
+  }
+  // return NextResponse.json({DIY:'All categories'})
 };
 
 export async function POST (req:NextRequest){
-  return NextResponse.json({DIY:'Category created'})
+  // return NextResponse.json({DIY:'Category created'})
+  const data = await req.json()
+  try {
+    const newCategory = await createCategory(data);
+    return res.json(newCategory,{status:201,statusText:'POST Category'})
+  } catch (error:any) {
+    return res.json({error:error.message})
+  }
 };
