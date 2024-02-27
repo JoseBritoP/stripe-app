145a6521aca476874a2b36d44ef0efa424f629c1
José Brito <josealbritose@gmail.com>
2024-02-27 11:14:07 -0300

    Constants navbar

---
+++ src/app/constants/index.ts
@@ -0,0 +1,33 @@
+export const navbarLinks = [
+  {
+    route:'/',
+    label:'Home'
+  },
+  {
+    route:'/auth',
+    label:'Sesión'
+  },
+  {
+    route:'/about',
+    label:'About Us'
+  },
+]
+
+export const navbarLinksLogin = [
+  {
+    route:'/',
+    label:'Home'
+  },
+  {
+    route:'/tasks',
+    label:'Tasks'
+  },
+  {
+    route:'/about',
+    label:'About Us'
+  },
+  {
+    route:'/newpost',
+    label:'Create Post'
+  },
+]
