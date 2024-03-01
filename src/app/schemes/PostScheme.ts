f7d3b73cdcf144a17ec6c36e4dabf9c693dd6b9d
José Brito <josealbritose@gmail.com>
2024-03-01 04:31:16 -0300

    Post scheme

---
+++ src/app/schemes/PostScheme.ts
@@ -0,0 +1,14 @@
+import z from 'zod';
+
+export const postScheme = z.object({
+  title:z.string(),
+  content:z.string(),
+  userId:z.number().int()
+});
+
+export const postSchemeV2 = z.object({
+  title:z.string(),
+  content:z.string(),
+  userId:z.number().int(),
+  categories:z.array(z.number()),
+});
