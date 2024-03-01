c80e7814bc14b1a1a033621fb4ec67cdcf586104
José Brito <josealbritose@gmail.com>
2024-03-01 04:31:07 -0300

    Post form hook

---
+++ src/app/hooks/usePostForm.tsx
@@ -0,0 +1,36 @@
+"use client"
+import { useState} from 'react'
+import { useRouter } from 'next/navigation'
+import { useFormik } from 'formik';
+import { postScheme } from '../schemes/PostScheme';
+import z from 'zod';
+
+
+type PostValues = z.infer<typeof postScheme>
+export default function usePostForm() {
+  const [message, setMessage] = useState('');
+  const [error, setError] = useState(false);
+  const [submit,setSubmit] = useState(false);
+
+  const router = useRouter();
+
+  const formik = useFormik<PostValues>({
+    initialValues: { title:'',content:'',userId:-1 },
+    onSubmit: async (values) => {
+      console.log(values)
+    },
+
+    validate: (values) => {
+      const result = postScheme.safeParse(values);
+      if (result.success) return;
+      const errors: Record<string, string> = {};
+      result.error.issues.forEach((error:any) => {
+        errors[error.path[0]] = error.message;
+      });
+      return errors;
+    },
+    validateOnBlur: true,
+  });
+
+  return { message, error, submit, formik }
+}
