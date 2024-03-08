543fc04793049d7b9aec78fd443857b752e61630
José Brito <josealbritose@gmail.com>
2024-03-08 16:55:38 -0300

    Post section TODO

--- src/app/(root)/posts/page.tsx
+++ src/app/(root)/posts/page.tsx
@@ -1,5 +1,5 @@
 import CardsContainer from "@/app/components/cards/CardsContainer";
-
+//TODO - Post section 
 export default function Posts(){
   return (
     <section className="flex flex-col justify-center items-center h-full w-full gap-y-5 mt-6">
