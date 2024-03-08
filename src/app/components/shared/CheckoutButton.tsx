9cd0d134f45a5ac1752013400a7aeb4418cd194d
José Brito <josealbritose@gmail.com>
2024-03-08 16:37:21 -0300

    Agree userId to premium

--- src/app/components/shared/CheckoutButton.tsx
+++ src/app/components/shared/CheckoutButton.tsx
@@ -1,14 +1,22 @@
 "use client"
 import React from 'react'
 import axios from 'axios';
+import { useAuthStore } from '@/app/services/authStore';
 
 export default function CheckoutButton({priceId}:{priceId:string}) {
 
+  const { profile } = useAuthStore();
+  
   const handleCheckout = async (priceId:string) => {
     // TODO: Agree userId to premium
     const response = await axios.post('/api/v1/checkout',{
-      priceId
+      priceId,
+      userId:profile.id
     })
+
+    if(response.status === 400){
+      window.location.href = '/'
+    }
 
     window.location.href = response.data.url
  
