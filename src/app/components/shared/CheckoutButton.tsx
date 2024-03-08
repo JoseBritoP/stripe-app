e2f81c90b3c131c43897b733c58637ebcc31e012
José Brito <josealbritose@gmail.com>
2024-03-08 13:30:15 -0300

    Checkout button

---
+++ src/app/components/shared/CheckoutButton.tsx
@@ -0,0 +1,22 @@
+"use client"
+import React from 'react'
+import axios from 'axios';
+
+export default function CheckoutButton({priceId}:{priceId:string}) {
+
+  const handleCheckout = async (priceId:string) => {
+    const response = await axios.post('/api/v1/checkout',{
+      priceId
+    })
+
+    window.location.href = response.data.url
+ 
+  }
+
+  return (
+    <button className='bg-sky-500 text-white px-4 py-2 rounded-sm'
+      onClick={() => handleCheckout(priceId)}
+    >Buy</button>
+
+  )
+}
