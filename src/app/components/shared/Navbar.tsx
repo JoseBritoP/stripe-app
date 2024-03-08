3070460c900baeb0334049759b9119a6e0b28d82
José Brito <josealbritose@gmail.com>
2024-03-08 13:29:16 -0300

    Pending Navbar fix

--- src/app/components/shared/Navbar.tsx
+++ src/app/components/shared/Navbar.tsx
@@ -8,9 +8,9 @@ const Navbar = () => {
   
   const pathname = usePathname();
   const router = useRouter();
-  const sessionString = localStorage.getItem('session');
-// Convertir los datos del usuario de cadena JSON a objeto
-  const session = sessionString && JSON.parse(sessionString)
+//   const sessionString = localStorage.getItem('session');
+// // Convertir los datos del usuario de cadena JSON a objeto
+//   const session = sessionString && JSON.parse(sessionString)
 
   const handleLogout = () => {
     localStorage.clear()
@@ -19,7 +19,7 @@ const Navbar = () => {
   return (
     <nav className='flex justify-center items-center'>
       <ul className="flex gap-x-5 text-lg justify-center items-center">
-       { session === null ?  navbarLinks.map((link)=>{
+       {/* { session === null ?  navbarLinks.map((link)=>{
             const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
             return (
               <li key={link.label}>
@@ -39,7 +39,14 @@ const Navbar = () => {
               <button onClick={handleLogout} className="hover:bg-sky-800 dark:hover:bg-sky-700 p-3 rounded-lg font-semibold text-gray-300 hover:text-gray-200 transition duration-150 ease-in"> Logout</button>
             </>
           )
-        }
+        } */}
+          {navbarLinksLogin.map((link)=>{
+                  const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+                  return (
+                    <li key={link.label}>
+                      <Link href={link.route} className={`hover:bg-slate-800 dark:hover:bg-slate-700 p-3 rounded-lg font-semibold text-gray-300 hover:text-gray-200 transition duration-150 ease-in ${isActive && 'bg-slate-900 dark:bg-slate-500 text-white'}`}>{link.label}</Link>
+                    </li>
+                  )})}
       </ul>
     </nav>
   )
